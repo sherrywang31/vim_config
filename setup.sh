34ccfe620366d9a1e80b82ffc33b7a5dcbe069e4
Sherry Wang <sherry.wang531@gmail.com>
2022-02-24 20:13:15 +0000

    remove sudo

--- setup.sh
+++ setup.sh
@@ -2,17 +2,17 @@ cp .vimrc ~/
 vim -c PlugInstall -c qall
 
 #YCM
-sudo apt install build-essential cmake
+apt install build-essential cmake
 git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/plugged/
 cd ~/.vim/plugged/YouCompleteMe
 python3 install.py
 
 #ctags
-sudo apt-get install ctags
+apt-get install ctags
 
 #pip
-sudo apt-get update
-sudo apt-get install python3-pip
+apt-get update
+apt-get install python3-pip
 
 # skelebot
-sudo pip3 install skelebot
+#pip3 install skelebot
